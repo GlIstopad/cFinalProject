d36aea564671dfdf98c90b2b8b930c711087f88a
GlIstopad <93276496+GlIstopad@users.noreply.github.com>
2026-05-10 09:56:59 +0300

    Comment out createPlayer function in player.c
    Comment out the createPlayer function definition.

--- 2Dframework/src/player.c
+++ 2Dframework/src/player.c
@@ -1,6 +1,6 @@
 #include <2Dframework/player.h>
 
-Player createPlayer(const char* image, int colorType, float maxVelocity, float accelaration, float modelSize[2], TexColumn walkAnim, TexColumn jumpAnim, float xCoord, float yCoord, float width, float height) {
+/*Player createPlayer(const char* image, int colorType, float maxVelocity, float accelaration, float modelSize[2], TexColumn walkAnim, TexColumn jumpAnim, float xCoord, float yCoord, float width, float height) {
   Player player;
   player.maxVelocity = maxVelocity;
   player.accelaration = accelaration;
@@ -13,7 +13,7 @@ Player createPlayer(const char* image, int colorType, float maxVelocity, float a
     2,
     0,
     1
-  }
+  }*/
   player.entity.obj = createGameObject(image, colorType, createEntityMesh(player.entity.model.modelsize),  xCoord, yCoord, width, height, 0.0f);
   entityUpdateTex(&player.entity);
 
